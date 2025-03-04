ed552763e84e398337fc4028ca715003c4e56e0c
Divyansh Pandey <divyanshpandey242@gmail.com>
2025-03-04 22:17:41 +0530

    Delete Flood-Monitoring-System-react directory

--- Flood-Monitoring-System-react/flood-monitoring/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/pages/**/*.{html,js,jsx}",  // Adjust this to point to your files in src/pages
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
